1b669b3948662730e8792102d871bd9e8b4d9884
Coen Schutte <coenschutte@gmail.com>
2019-05-31 19:42:11 +0200

    fixed parameters, added --help

--- Main.java
+++ Main.java
@@ -15,46 +15,58 @@ public class Main {
 
     public static boolean nowalls;
     public static boolean pride;
+    public static boolean help;
 
     public static void main(String[] args) {
 
         nowalls = false;
         pride = false;
+        help = false;
 
-        if (args.length > 0) {
-            if (args[0].equals("--no-walls")) {
-                nowalls = true;
-            }
-            if(args[0]. equals("--pride")){
-                pride = true;
+        for(int i = 0; i < args.length; i++) {
+            System.out.println(i);
+            if (args.length > 0) {
+                if (args[i].equals("--no-walls")) {
+                    nowalls = true;
+                }
+                if (args[i].equals("--pride")) {
+                    pride = true;
+                }
+                if(args[i].equals("--help")){
+                    System.out.println("The following parameters are available:");
+                    System.out.println("\"--no-walls\" this disables all walls");
+                    System.out.println("\"--pride\" this turns the snake into a rainbow snake");
+                    help = true;
+                }
             }
         }
 
-        /* unit test */
-        Head head = new Head(new Point(6, 4), Color.BLACK);
+        if(!help) {
+            /* unit test */
+            Head head = new Head(new Point(6, 4), Color.BLACK);
 
-        Snake snake = new Snake(head, nowalls, pride);
-        Body body = new Body(new Point(5, 4), Color.BLUE);
+            Snake snake = new Snake(head, nowalls, pride);
+            Body body = new Body(new Point(5, 4), Color.BLUE);
 
-        snake.append(body);
+            snake.append(body);
 
-        /* screen */
-        JFrame frame = new JFrame();
-        Game game = new Game(snake);
-        frame.add(game);
-        frame.addKeyListener(game);
-        frame.pack();
-        frame.setVisible(true);
+            /* screen */
+            JFrame frame = new JFrame();
+            Game game = new Game(snake);
+            frame.add(game);
+            frame.addKeyListener(game);
+            frame.pack();
+            frame.setVisible(true);
 
-        task = new TimerTask() {
+            task = new TimerTask() {
 
-            @Override
-            public void run() {
-                snake.move();
-                game.repaint();
-            }
-        };
-        timer.schedule(task, 100, 100);
-
+                @Override
+                public void run() {
+                    snake.move();
+                    game.repaint();
+                }
+            };
+            timer.schedule(task, 100, 100);
+        }
     }
 }
